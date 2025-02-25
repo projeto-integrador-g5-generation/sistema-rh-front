0e636e7a8c1fbb28e11dd84d0452e5e6cfb9d512
Vinicius Rodrigues <rodrigues.vini.2004@gmail.com>
2025-02-25 17:57:10 -0300

    Merge 'feat/navbar_footer' into feat/rotas

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,26 +1,28 @@
-import { GithubLogo } from "@phosphor-icons/react"
+import {GithubLogo} from "@phosphor-icons/react"
 
 function Footer() {
+  const data = new Date().getFullYear();
 
-    let data = new Date().getFullYear()
-
-    return (
-        <>
-            <div className="flex justify-center bg-indigo-800 text-white">
-                <div className="container flex flex-col items-center py-4">
-                    <p className='text-xl font-bold'>
-                        Gestão PRO | Copyright: {data}
-                    </p>
-                    <p className='text-lg text-center'>Acesse nossa rede social e conheça nossos projetos</p>
-                    <div className='flex gap-2'>
-                        <a href="https://github.com/projeto-integrador-g5-generation/" target='_blank'>
-                            <GithubLogo size={48} weight='bold' />
-                        </a>                        
-                    </div>
-                </div>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="flex justify-center bg-indigo-800 text-white">
+        <div className="container flex flex-col items-center py-4">
+          <p className="text-xl font-bold">Gestão PRO | Copyright: {data}</p>
+          <p className="text-lg text-center">
+            Acesse nossa rede social e conheça nossos projetos
+          </p>
+          <div className="flex gap-2">
+            <a
+              href="https://github.com/projeto-integrador-g5-generation/"
+              target="_blank"
+            >
+              <GithubLogo size={48} weight="bold" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Footer
+export default Footer;
